76db3fc6b34dd00531e927048c1a2009a03fdf2d
Priyanthan <priyanthan2@gmail.com>
2024-12-16 01:14:29 +0000

    Refactored Methods
    Add separate methods for handle add customer and parcel

--- Assingment2/src/main/java/com/mycompany/assingment2/Manager.java
+++ Assingment2/src/main/java/com/mycompany/assingment2/Manager.java
@@ -18,6 +18,13 @@ import java.util.Iterator;
  * @author priya
  */
 public class Manager {
+    // Create Gloabal Customer QUeue
+    // Customer Queue
+    static QueueofCustomers cus_queue = new QueueofCustomers();
+    // Initialize Parcel
+    static ParcelMap parcelMap = new ParcelMap();
+    
+    
     
     // Load the data
     public static Queue Load_data(String fileName)
@@ -30,7 +37,7 @@ public class Manager {
           
             while((line = my_reader.readLine()) != null)
             {
-                
+                // Add to the log
                 Log.getInstance().addEvent("System Loaded Data: " + line);
                 
                 // split the line into parts
@@ -43,10 +50,6 @@ public class Manager {
                 {
                     Log.getInstance().addEvent("Missing Details in the Source file");
                 }
-                
-                
-                
-                //System.out.println(line);
             }
         }
         catch(Exception e)
@@ -54,73 +57,35 @@ public class Manager {
             Log.getInstance().addEvent("Somthing went wrong");
             Log.getInstance().addEvent(e.getMessage());
         }
-        return cus_queue;
-        
+        return cus_queue; 
     }
 
-    public static void main(String[] args) {
+       
+    public static void prerequisite()
+    {
         // Customer File path
         String cus_fileName = "C:/Users/priya/OneDrive - University of Hertfordshire/Modules/Software Arch/Assignment/Assignment_2/Assingment2/src/main/java/com/mycompany/assingment2/Custs.csv";
         // Parcel File path
         String par_fileName = "C:/Users/priya/OneDrive - University of Hertfordshire/Modules/Software Arch/Assignment/Assignment_2/Assingment2/src/main/java/com/mycompany/assingment2/Parcels.csv";
         
-        
-        /*
-        System.out.println("Hello World!");
-        
-        // Create a parcel
-        Parcel parcel = new Parcel("P001", Parcel.Status.PENDING,500,10.5,LocalDate.of(2024,12,14));
-        
-        // Display parcel details
-        System.out.println(parcel);
-        
-        // Check if the parcel is valid
-        System.out.println("Valid Request: "+ parcel.validRequest());
-        
-        // Update Parcel Status
-        parcel.setStatus(Parcel.Status.COLLECTED);
-        
-        // Calculate storage fee
-        System.out.println("Storage Fee: £" + parcel.calculateSorageFee());
-        */
         // Customer List
         Queue<String[]> customerDetails;
-        // Customer Queue
-        QueueofCustomers cus_queue = new QueueofCustomers();
         // Get Customer Details from csv file
         customerDetails = Load_data(cus_fileName);
         // Iterate the details till end of the data.
         while(!customerDetails.isEmpty())
         {
-            
-            String[] cus_det = customerDetails.poll();
-            // Create customer
-            cus_queue.addCustomerToQueue(new Customer(cus_det[0],cus_det[1],cus_det[2],cus_det[3]));
-                     
-            
-            //System.out.println("Customer Name: "+cus_det[0]);
-            //System.out.println("Customer Info: "+cus_det[1]);
-            //System.out.println("Customer ID: "+cus_det[2]);
-            //System.out.println("Customer ID: "+cus_det[3]);
+            addCustomer(customerDetails);
         }
-        
         
         // Create Queue for parcel
         Queue<String[]> parcelDetails;
-        // Initialize Parcel
-        ParcelMap parcelMap = new ParcelMap();
         // Load the data into Queue
         parcelDetails = Load_data(par_fileName);
-       
         while(!parcelDetails.isEmpty())
         {
-            String[] par_det = parcelDetails.poll();
-            System.out.println("Value: "+Status.valueOf(par_det[1]));
-            
-            //parcelMap.addParcel(new Parcel(par_det[0],Status.valueOf(par_det[1]),int(par_det[2]),double(par_det[3]),LocalDate.parse(par_det[4])));
-            parcelMap.addParcel(new Parcel(par_det[0],Status.valueOf(par_det[1]),Integer.parseInt(par_det[2]),Double.parseDouble(par_det[3]),LocalDate.parse(par_det[4])));
-                
-         }
+            addParcel(parcelDetails);
+        }
             
             
         /*
@@ -130,9 +95,6 @@ public class Manager {
             System.out.println(it.next());
         }
         */
-        
-        
-        
         
         /*
         // Ini componennts
@@ -153,6 +115,35 @@ public class Manager {
         worker.processNextCustomer();*/
         
         // Write log to file
+    }
+    /*
+    TODO: Add Customer - X
+    TODO: Remove Customer
+    TODO: Add Parcel - X
+    TODO: Release Parcel and put it in Collected Queue
+    */
+    public static void addCustomer(Queue<String[]> customerDetails)
+    {
+        String[] cus_det = customerDetails.poll();
+        // Create customer
+        cus_queue.addCustomerToQueue(new Customer(cus_det[0],cus_det[1],cus_det[2],cus_det[3]));  
+    }
+    
+    public static void addParcel(Queue<String[]> parcelDetails)
+    {
+        String[] par_det = parcelDetails.poll();
+        parcelMap.addParcel(new Parcel(par_det[0],Status.valueOf(par_det[1]),Integer.parseInt(par_det[2]),Double.parseDouble(par_det[3]),LocalDate.parse(par_det[4])));
+            
+    }
+    
+    
+    public static void main(String[] args) 
+    {
+        // Begin play
+        prerequisite();
+        
+        
+        
         String file_loc = "system_log.txt";
         Log.getInstance().writeLogToFile(file_loc);
     }
